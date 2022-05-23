c534689d357217c84fb907feee60e273cca412fc
chenkai02 <1445015751@qq.com>
2022-05-23 14:44:35 +0800

    feat:集成log4j2日志

--- app-web/src/main/java/com/exampl/demo/controller/TestController.java
+++ app-web/src/main/java/com/exampl/demo/controller/TestController.java
@@ -1,16 +1,14 @@
 package com.exampl.demo.controller;
-
 import com.exampl.demo.entity.TesUser;
 import com.exampl.demo.service.TestService;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
+@Log4j2
 public class TestController {
     @Autowired
     private TestService testService;
@@ -21,9 +19,19 @@ public class TestController {
     }
 
     @GetMapping("/getUsers")//获取User信息接口
-    public List<TesUser> query() {
+    public List<TesUser> getUsers() {
         List<TesUser> users = testService.getAll();
         return users;
+    }
+
+    //测试日志输出功能
+    @GetMapping("query")
+    public List<TesUser> query(){
+        log.info("===========>开始查询数据库");
+        List<TesUser> list = testService.getAll();
+        log.info("======>>>: 数据库查询完成");
+        System.out.println(list);
+        return list;
     }
 
 
