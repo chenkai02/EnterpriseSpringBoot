b86ec5c6a219cd3dee45a7474115c93afc24d5a0
chenkai02 <1445015751@qq.com>
2022-05-27 17:24:44 +0800

    feat:集成swagger2

--- app-web/src/main/java/com/exampl/demo/Application.java
+++ app-web/src/main/java/com/exampl/demo/Application.java
@@ -1,10 +1,12 @@
 package com.exampl.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @MapperScan("com.exampl.app.mapper")
 @SpringBootApplication
+@EnableSwagger2
 public class Application {
     public static void main(String[] args){
         SpringApplication.run(Application.class,args);
--- app-web/src/main/java/com/exampl/demo/controller/NewFeatureController.java
+++ app-web/src/main/java/com/exampl/demo/controller/NewFeatureController.java
@@ -2,15 +2,18 @@ package com.exampl.demo.controller;
 
 import com.exampl.demo.entity.TesUser;
 import com.exampl.demo.service.TestService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNullApi;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
+@Api(tags = "stream测试类")
 public class NewFeatureController {
 
     @Autowired
@@ -25,6 +28,7 @@ public class NewFeatureController {
      * @return result
      */
     @GetMapping("/filter")
+    @ApiOperation("filter测试")
     public List<String> newFeature(String number){
         List<String> result = strArr.stream().filter(str -> !number.equals(str)&&
                 !str.equals("")).collect(Collectors.toList());//去除输入的变量返回list
@@ -38,6 +42,7 @@ public class NewFeatureController {
      * @return names
      */
     @GetMapping("/map")
+    @ApiOperation("map测试")
     public List<String> newFeature(){
         List<TesUser> users = testService.getAll();
         List<String> names = users.stream().map(e -> e.getName()).collect(Collectors.toList());//只返回对象的名字属性
